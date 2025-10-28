8b87853e92c1cd5aefb0b7a1cbb639d19608ad93
LincolnSand <sandlincoln@gmail.com>
2025-10-28 12:57:12 -0400

    Fix alpha equivalence function
    Before, the following two checks would erroneously fail:
    
    (check-same (Pair (-> Nat Nat) (-> Nat Nat)) (cons (the (-> Nat Nat) (lambda (x) x)) (the (-> Nat Nat) (lambda (y) y))) (cons (the (-> Nat Nat) (lambda (z) z)) (the (-> Nat Nat) (lambda (w) w))))
    
    (TestProgram9.pie)
    
    and:
    
    (check-same (-> (-> Nat Nat) Nat Nat) (the (-> (-> Nat Nat) Nat Nat) (lambda (x y) (x y))) (the (-> (-> Nat Nat) Nat Nat) (lambda (a b) (a b))))
    
    (TestProgram11.pie)
    
    TODO: A non-termination bug has been discovered when running the following Pie program as input:
    
    (check-same Nat (the (-> (-> Nat Nat) Nat) (lambda (x y) (x y))) (the (-> (-> Nat Nat) Nat) (lambda (a b) (a b))))
    
    (TestProgram10.pie)
    
    The program should be rejected as a type error, but instead it causes an infinite loop within the implementation.

--- runTests.sh
+++ runTests.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-cabal run unofficial-pie-hs -- tests/pie/TestProgram1.pie tests/pie/TestProgram2.pie tests/pie/TestProgram3.pie tests/pie/TestProgram4.pie tests/pie/TestProgram5.pie tests/pie/TestProgram6.pie tests/pie/TestProgram7.pie tests/pie/TestProgram8.pie tests/pie/OnePlusOneEqualsTwo.pie tests/pie/OneNeqZero.pie
+# cabal run unofficial-pie-hs -- tests/pie/TestProgram1.pie tests/pie/TestProgram2.pie tests/pie/TestProgram3.pie tests/pie/TestProgram4.pie tests/pie/TestProgram5.pie tests/pie/TestProgram6.pie tests/pie/TestProgram7.pie tests/pie/TestProgram8.pie tests/pie/OnePlusOneEqualsTwo.pie tests/pie/OneNeqZero.pie
+
+cabal run unofficial-pie-hs -- tests/pie/TestProgram10.pie
--- src/Typing/TypingRules.hs
+++ src/Typing/TypingRules.hs
@@ -401,7 +401,92 @@ alphaEquivImpl _ b1 b2 (CoreVar x) (CoreVar y) =
         (Just lvlX, Just lvlY) -> lvlX == lvlY
         (Nothing, Nothing) -> x == y
         (_, _) -> False
-alphaEquivImpl _ _ _ e1 e2 = e1 == e2
+alphaEquivImpl lvl b1 b2 (CoreThe (The t1 e1)) (CoreThe (The t2 e2)) =
+    (alphaEquivImpl lvl b1 b2 t1 t2) && (alphaEquivImpl lvl b1 b2 e1 e2)
+alphaEquivImpl _ _ _ CoreAtom CoreAtom = True
+alphaEquivImpl _ _ _ (CoreAtomLiteral a1) (CoreAtomLiteral a2) = a1 == a2
+alphaEquivImpl lvl b1 b2 (CoreCons car1 cdr1) (CoreCons car2 cdr2) =
+    (alphaEquivImpl lvl b1 b2 car1 car2) && (alphaEquivImpl lvl b1 b2 cdr1 cdr2)
+alphaEquivImpl lvl b1 b2 (CoreCar p1) (CoreCar p2) =
+    alphaEquivImpl lvl b1 b2 p1 p2
+alphaEquivImpl lvl b1 b2 (CoreCdr p1) (CoreCdr p2) =
+    alphaEquivImpl lvl b1 b2 p1 p2
+alphaEquivImpl lvl b1 b2 (CoreApplication f arg1) (CoreApplication g arg2) =
+    (alphaEquivImpl lvl b1 b2 f g) && (alphaEquivImpl lvl b1 b2 arg1 arg2)
+alphaEquivImpl _ _ _ CoreNat CoreNat = True
+alphaEquivImpl _ _ _ CoreNatZero CoreNatZero = True
+alphaEquivImpl lvl b1 b2 (CoreNatAdd1 n1) (CoreNatAdd1 n2) =
+    alphaEquivImpl lvl b1 b2 n1 n2
+alphaEquivImpl lvl b1 b2 (CoreWhichNat target1 (The baseType1 baseExpr1) step1) (CoreWhichNat target2 (The baseType2 baseExpr2) step2) =
+    (alphaEquivImpl lvl b1 b2 target1 target2) && (alphaEquivImpl lvl b1 b2 baseType1 baseType2)
+    && (alphaEquivImpl lvl b1 b2 baseExpr1 baseExpr2) && (alphaEquivImpl lvl b1 b2 step1 step2)
+alphaEquivImpl lvl b1 b2 (CoreIterNat target1 (The baseType1 baseExpr1) step1) (CoreIterNat target2 (The baseType2 baseExpr2) step2) =
+    (alphaEquivImpl lvl b1 b2 target1 target2) && (alphaEquivImpl lvl b1 b2 baseType1 baseType2)
+    && (alphaEquivImpl lvl b1 b2 baseExpr1 baseExpr2) && (alphaEquivImpl lvl b1 b2 step1 step2)
+alphaEquivImpl lvl b1 b2 (CoreRecNat target1 (The baseType1 baseExpr1) step1) (CoreRecNat target2 (The baseType2 baseExpr2) step2) =
+    (alphaEquivImpl lvl b1 b2 target1 target2) && (alphaEquivImpl lvl b1 b2 baseType1 baseType2)
+    && (alphaEquivImpl lvl b1 b2 baseExpr1 baseExpr2) && (alphaEquivImpl lvl b1 b2 step1 step2)
+alphaEquivImpl lvl b1 b2 (CoreIndNat target1 motive1 base1 step1) (CoreIndNat target2 motive2 base2 step2) =
+    (alphaEquivImpl lvl b1 b2 target1 target2) && (alphaEquivImpl lvl b1 b2 motive1 motive2)
+    && (alphaEquivImpl lvl b1 b2 base1 base2) && (alphaEquivImpl lvl b1 b2 step1 step2)
+alphaEquivImpl lvl b1 b2 (CoreList elementType1) (CoreList elementType2) =
+    alphaEquivImpl lvl b1 b2 elementType1 elementType2
+alphaEquivImpl _ _ _ CoreListNil CoreListNil = True
+alphaEquivImpl lvl b1 b2 (CoreListColonColon h1 t1) (CoreListColonColon h2 t2) =
+    (alphaEquivImpl lvl b1 b2 h1 h2) && (alphaEquivImpl lvl b1 b2 t1 t2)
+alphaEquivImpl lvl b1 b2 (CoreRecList target1 (The baseType1 baseExpr1) step1) (CoreRecList target2 (The baseType2 baseExpr2) step2) =
+    (alphaEquivImpl lvl b1 b2 target1 target2) && (alphaEquivImpl lvl b1 b2 baseType1 baseType2)
+    && (alphaEquivImpl lvl b1 b2 baseExpr1 baseExpr2) && (alphaEquivImpl lvl b1 b2 step1 step2)
+alphaEquivImpl lvl b1 b2 (CoreIndList target1 motive1 base1 step1) (CoreIndList target2 motive2 base2 step2) =
+    (alphaEquivImpl lvl b1 b2 target1 target2) && (alphaEquivImpl lvl b1 b2 motive1 motive2)
+    && (alphaEquivImpl lvl b1 b2 base1 base2) && (alphaEquivImpl lvl b1 b2 step1 step2)
+alphaEquivImpl lvl b1 b2 (CoreVec elementType1 len1) (CoreVec elementType2 len2) =
+    (alphaEquivImpl lvl b1 b2 elementType1 elementType2) && (alphaEquivImpl lvl b1 b2 len1 len2)
+alphaEquivImpl _ _ _ CoreVecNil CoreVecNil = True
+alphaEquivImpl lvl b1 b2 (CoreVecColonColon h1 t1) (CoreVecColonColon h2 t2) =
+    (alphaEquivImpl lvl b1 b2 h1 h2) && (alphaEquivImpl lvl b1 b2 t1 t2)
+alphaEquivImpl lvl b1 b2 (CoreHead vec1) (CoreHead vec2) =
+    alphaEquivImpl lvl b1 b2 vec1 vec2
+alphaEquivImpl lvl b1 b2 (CoreTail vec1) (CoreTail vec2) =
+    alphaEquivImpl lvl b1 b2 vec1 vec2
+alphaEquivImpl lvl b1 b2 (CoreIndVec n1 target1 motive1 base1 step1) (CoreIndVec n2 target2 motive2 base2 step2) =
+    (alphaEquivImpl lvl b1 b2 n1 n2) && (alphaEquivImpl lvl b1 b2 target1 target2)
+    && (alphaEquivImpl lvl b1 b2 motive1 motive2) && (alphaEquivImpl lvl b1 b2 base1 base2)
+    && (alphaEquivImpl lvl b1 b2 step1 step2)
+alphaEquivImpl lvl b1 b2 (CoreEq eqType1 from1 to1) (CoreEq eqType2 from2 to2) =
+    (alphaEquivImpl lvl b1 b2 eqType1 eqType2) && (alphaEquivImpl lvl b1 b2 from1 from2)
+    && (alphaEquivImpl lvl b1 b2 to1 to2)
+alphaEquivImpl lvl b1 b2 (CoreEqSame expr1) (CoreEqSame expr2) =
+    alphaEquivImpl lvl b1 b2 expr1 expr2
+alphaEquivImpl lvl b1 b2 (CoreSymm expr1) (CoreSymm expr2) =
+    alphaEquivImpl lvl b1 b2 expr1 expr2
+alphaEquivImpl lvl b1 b2 (CoreCong target1 coDomainType1 func1) (CoreCong target2 coDomainType2 func2) =
+    (alphaEquivImpl lvl b1 b2 target1 target2) && (alphaEquivImpl lvl b1 b2 coDomainType1 coDomainType2)
+    && (alphaEquivImpl lvl b1 b2 func1 func2)
+alphaEquivImpl lvl b1 b2 (CoreReplace target1 motive1 base1) (CoreReplace target2 motive2 base2) =
+    (alphaEquivImpl lvl b1 b2 target1 target2) && (alphaEquivImpl lvl b1 b2 motive1 motive2)
+    && (alphaEquivImpl lvl b1 b2 base1 base2)
+alphaEquivImpl lvl b1 b2 (CoreTrans fromMid1 midTo1) (CoreTrans fromMid2 midTo2) =
+    (alphaEquivImpl lvl b1 b2 fromMid1 fromMid2) && (alphaEquivImpl lvl b1 b2 midTo1 midTo2)
+alphaEquivImpl lvl b1 b2 (CoreIndEq target1 motive1 base1) (CoreIndEq target2 motive2 base2) =
+    (alphaEquivImpl lvl b1 b2 target1 target2) && (alphaEquivImpl lvl b1 b2 motive1 motive2)
+    && (alphaEquivImpl lvl b1 b2 base1 base2)
+alphaEquivImpl lvl b1 b2 (CoreEither leftType1 rightType1) (CoreEither leftType2 rightType2) =
+    (alphaEquivImpl lvl b1 b2 leftType1 leftType2) && (alphaEquivImpl lvl b1 b2 rightType1 rightType2)
+alphaEquivImpl lvl b1 b2 (CoreEitherLeft left1) (CoreEitherLeft left2) =
+    alphaEquivImpl lvl b1 b2 left1 left2
+alphaEquivImpl lvl b1 b2 (CoreEitherRight right1) (CoreEitherRight right2) =
+    alphaEquivImpl lvl b1 b2 right1 right2
+alphaEquivImpl lvl b1 b2 (CoreIndEither target1 base1 leftStep1 rightStep1) (CoreIndEither target2 base2 leftStep2 rightStep2) =
+    (alphaEquivImpl lvl b1 b2 target1 target2) && (alphaEquivImpl lvl b1 b2 base1 base2)
+    && (alphaEquivImpl lvl b1 b2 leftStep1 leftStep2) && (alphaEquivImpl lvl b1 b2 rightStep1 rightStep2)
+alphaEquivImpl _ _ _ CoreTrivial CoreTrivial = True
+alphaEquivImpl _ _ _ CoreTrivialSole CoreTrivialSole = True
+alphaEquivImpl _ _ _ CoreAbsurd CoreAbsurd = True
+alphaEquivImpl lvl b1 b2 (CoreIndAbsurd target1 motive1) (CoreIndAbsurd target2 motive2) =
+    (alphaEquivImpl lvl b1 b2 target1 target2) && (alphaEquivImpl lvl b1 b2 motive1 motive2)
+alphaEquivImpl _ _ _ CoreU CoreU = True
+alphaEquivImpl _ _ _ _ _ = False -- mismatched `CoreTerm` constructors
 
 lookupBinding :: Bindings -> Name -> Maybe Int
 lookupBinding [] _ = Nothing
